7828e5acc9f27d68ff10818b8f3d5642dadf049b
Efe Gerek <efegerek10@gmail.com>
2025-04-04 14:33:35 +0300

    Finished the program to calculate the interest

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # interest_rate_calculator
-This Python program let's you calculate your interest rate
+This Python program let's you calculate your interest rate.
--- calculator.py
+++ calculator.py
@@ -1,1 +1,14 @@
+#Greeting the user to the program.
 print("Welcome to interest rate calculator! Let me help you calculate your interest rate.")
+
+#Asking for the inputs to calculate the interest rate.
+total_amount = float(input("How much is the total amount?"))
+interest_rate = float(input("How much is the monthly interest rate?"))
+month = int(input("In how many months do you want to payback?"))
+
+#Calculating the monthly payment and total interest payment.
+monthly_payment = int((total_amount * (1 + interest_rate / 100)) - total_amount)
+total_payment = int((monthly_payment * month))
+
+print("Your monthly payment will be {m}.Your total interest payment will be {t}.".format(m = monthly_payment,t = total_payment))
+print("Thank you for using the interest rate calculator!")
